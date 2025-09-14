0c18c9c7ee35c0f6c606b7d46e7874674c4c3d79
Leo Z <3324234+leozusa@users.noreply.github.com>
2025-09-14 00:57:35 -0400

    Task 2

--- backend/BalanceHub.API/obj/Debug/net9.0/BalanceHub.API.AssemblyInfo.cs
+++ backend/BalanceHub.API/obj/Debug/net9.0/BalanceHub.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BalanceHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd0f2bc16f7fc4aa25e121d5898654fb12da048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf1c1ed9af064ea067e35ceeeb475ebbe1cea14")]
 [assembly: System.Reflection.AssemblyProductAttribute("BalanceHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BalanceHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
